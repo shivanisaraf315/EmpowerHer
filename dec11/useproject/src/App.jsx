719ff022a554352ea41a7fa309eb6513cea4aa60
shivanisaraf315 <shivanisaraf3105@gmail.com>
2025-12-15 10:27:59 +0530

    dec 11 assignements are commited

---
+++ dec11/useproject/src/App.jsx
@@ -0,0 +1,13 @@
+import UserData from "./UserData";
+import Counter from "./counter";
+
+function App() {
+  return (
+    <>
+      <UserData />
+      <Counter />
+    </>
+  );
+}
+
+export default App;
